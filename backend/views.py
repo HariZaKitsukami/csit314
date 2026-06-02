9441cdf5a6195f81a95ec70382021d900b060de0
Braydn <101867327+BraydN306@users.noreply.github.com>
2026-06-02 20:55:30 +1000

    Add job and candidate search views with JSON response

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,10 @@
 from site import register_readline
 from django.contrib.auth import authenticate, login as lin
 from backend.models import User
-from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.http import HttpRequest, HttpResponse, HttpResponseForbidden, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from .services import *
+
 
 # Create your views here.
 def login(request: HttpRequest) -> HttpResponse:
@@ -23,3 +25,85 @@ def register(request: HttpRequest) -> HttpResponse:
     user.save()
     lin(request, user)
     return redirect("dashboard")
+
+# Create your views here.
+def jobs_list(request):
+    jobs = search_jobs()
+
+    data = []
+
+    for job in jobs:
+        data.append({
+            #"id": job.id,
+            "job_title": job.job_location,
+            "job_location": job.job_location,
+            "work_mode": job.work_mode,
+        })
+    
+    return JsonResponse(data, safe=False)
+
+
+def jobs_search(request):
+
+    keyword = request.GET.get("keyword")
+    location = request.GET.get("location")
+    work_mode = request.GET.get("work_mode")
+    data = []
+
+    jobs = search_jobs(
+        keyword=keyword,
+        location=location,
+        work_mode=work_mode
+    )
+
+
+    for job in jobs:
+       data.append({
+           #"id": job.id,
+           "job_title": job.job_title,
+           "job_location": job.job_location,
+           "work_mode": job.work_mode,
+       })
+       
+    return JsonResponse(data, safe=False)
+
+
+def candidates_search(request):
+
+    keyword = request.GET.get("keyword")
+    location = request.GET.get("location")
+    education = request.GET.get("education")
+
+    candidates = search_candidates(
+        keyword=keyword,
+        location=location,
+        education=education
+    )
+
+    data = []
+
+    for candidate in candidates:
+        data.append({
+            "id": candidate.id,
+            "name": candidate.full_name,
+            "education": candidate.education,
+            "location": candidate.location,
+        })
+
+    return JsonResponse(data, safe=False)
+
+
+def recommended_candidates(request, job_id):
+
+    matches = get_recom_candidates(job_id)
+
+    data = []
+
+    for candidate, score in matches:
+        data.append({
+            "candidate_id": candidate.id,
+            "candidate_name": candidate.full_name,
+            "score": score
+        })
+
+    return JsonResponse(data, safe=False)
