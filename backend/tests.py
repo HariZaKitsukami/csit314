5caf1024d8bdcafecb046a18dd87444e63252c4e
Heroinator <aidangreenlees@outlook.com>
2026-06-02 18:42:55 +1000

    Update tests.py

--- backend/tests.py
+++ backend/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
-from backend.migrations.models import services.*;
-from ui-prototype-joe-/frontend import jobMatchingClasses.*;
+from backend.migrations.models import services;
+from ui-prototype-joe-/frontend import jobMatchingClasses;
 
 class scoreTestCase(TestCase)
-    candidate1 = CandidateProfile("", "bachelors", "computer science", 2, "remote", "Wollongong" );
-    candidate2 = CandidateProfile("", "bachelors", "computer science", 1, "in-person", "Wollongong");
-    job1 = JobListing("", "", "bachelors", "", 2, "Remote", "Wollongong");
-    self.assertEqual(services.cal_match_score(candidate1, job1), 30);
-    self.assertEqual(services.cal_match_score(candidate2, job2), 20);
+    def setUp(self):
+        candidate1 = CandidateProfile("", "bachelors", "computer science", 2, "remote", "Wollongong" );
+        candidate2 = CandidateProfile("", "bachelors", "computer science", 1, "in-person", "Wollongong");
+        job1 = JobListing("", "", "bachelors", "", 2, "Remote", "Wollongong");
+    def testAssertEqual(self):
+        self.assertEqual(services.cal_match_score(candidate1, job1), 30);
+        self.assertEqual(services.cal_match_score(candidate2, job2), 20);
