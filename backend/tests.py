7b0b05de3c6d2a232e4def959145b83b733d6b7c
Heroinator <aidangreenlees@outlook.com>
2026-06-02 23:24:32 +1000

    Update tests.py

--- backend/tests.py
+++ backend/tests.py
@@ -7,9 +7,9 @@ from backend.services import cal_match_score, search_jobs;
 class testCase(TestCase):
     def setUp(self):
         user1 = User.objects.create(role = "cand", is_member = False);
-        candidate1 = CandidateProfile.objects.create(user_id = 0, full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 2, location = "Wollongong", preferred_work_mode = "remote");
-        candidate2 = CandidateProfile.objects.create(user_id = 0, full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 3, location = "Wollongong", preferred_work_mode = "remote");
-        employer1 = EmployerProfile.objects.create(user_id = 0, company_name ="", location = "Wollongong", description = "")
+        candidate1 = CandidateProfile.objects.create(user_id = user1, full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 2, location = "Wollongong", preferred_work_mode = "remote");
+        candidate2 = CandidateProfile.objects.create(user_id = user1, full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 3, location = "Wollongong", preferred_work_mode = "remote");
+        employer1 = EmployerProfile.objects.create(user_id = user1, company_name ="", location = "Wollongong", description = "")
         job1 = JobListing.objects.create(employer_id = employer1.user_id, job_title = "computer programmer", job_description = "programming the computers", required_education = "bachelors",required_skills = "", required_experience = 2, work_mode = "Remote", job_location = "Wollongong", salary = "60.00", benefits = "");
         job1 = JobListing.objects.create(employer_id = employer1.user_id, job_title = "database programmer", job_description = "coding the database",required_education = "bachelors",required_skills = "", required_experience = 3, work_mode = "Remote", job_location = "Wollongong", salary = "60.00", benefits = "");
     def testScoreCase(self):
