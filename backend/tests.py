6b8c689284c621b78cd60d3dd3b191c9082e3b5e
Heroinator <aidangreenlees@outlook.com>
2026-06-02 18:37:21 +1000

    Update tests.py
    added unittest for scoreTestCase

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from backend.migrations.models import services.*;
+from ui-prototype-joe-/frontend import jobMatchingClasses.*;
+
+class scoreTestCase(TestCase)
+    candidate1 = CandidateProfile("", "bachelors", "computer science", 2, "remote", "Wollongong" );
+    candidate2 = CandidateProfile("", "bachelors", "computer science", 1, "in-person", "Wollongong");
+    job1 = JobListing("", "", "bachelors", "", 2, "Remote", "Wollongong");
+    self.assertEqual(services.cal_match_score(candidate1, job1), 30);
+    self.assertEqual(services.cal_match_score(candidate2, job2), 20);
