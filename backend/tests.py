ef03fa89a00cec77ee0284d4c2ad86e60e64c039
Heroinator <aidangreenlees@outlook.com>
2026-06-02 20:30:03 +1000

    Update tests.py
    updated scoreTestCase

--- backend/tests.py
+++ backend/tests.py
@@ -5,9 +5,10 @@ from backend.migrations.models import services, models;
 
 class scoreTestCase(TestCase)
     def setUp(self):
-        candidate1 = CandidateProfile("", "bachelors", "computer science", 2, "remote", "Wollongong" );
-        candidate2 = CandidateProfile("", "bachelors", "computer science", 1, "in-person", "Wollongong");
-        job1 = JobListing("", "", "bachelors", "", 2, "Remote", "Wollongong");
+        candidate1 = CandidateProfile.objects.create(full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 2, location = "Wollongong", preferred_work_mode = "remote");
+        candidate2 = CandidateProfile.objects.create(full_name = "", phone = "" ,education = "bachelors", major = "computer science", experience = 2, location = "Wollongong", preferred_work_mode = "remote");
+        employer1 = EmployerProfile.objects.create(company_name ="", location = "Wollongong", description = "")
+        job1 = JobListing.objects.create(employer_id = employer1.user_id, job_title = "",required_education = "bachelors",required_skills = "", required_experience = 2, work_mode = "Remote", job_location = "Wollongong", salary = "60.00", benefits = "");
     def testAssertEqual(self):
         self.assertEqual(services.cal_match_score(candidate1, job1), 30);
         self.assertEqual(services.cal_match_score(candidate2, job2), 20);
