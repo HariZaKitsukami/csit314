2e0cf2b1f6aa2cfb0850834215a6ea910bf7653b
Heroinator <aidangreenlees@outlook.com>
2026-06-03 15:30:37 +1000

    Update tests.py

--- backend/tests.py
+++ backend/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from backend.models import User, CandidateProfile, EmployerProfile, JobListing;
-from backend.services import cal_match_score, search_jobs, get_job_details;
+from backend.services import cal_match_score, search_jobs, get_job_details, search_candidates;
 
 class testCase(TestCase):
     def setUp(self):
